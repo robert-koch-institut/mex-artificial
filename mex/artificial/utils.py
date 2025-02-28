bfdc000180d6529941faea126143d463e64f1c23
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-02-28 09:36:49 +0100

    add util

---
+++ mex/artificial/utils.py
@@ -0,0 +1,7 @@
+def ensure_list(values: object) -> list[object]:
+    """Wrap single objects in lists, replace None with [] and return lists untouched."""
+    if values is None:
+        return []
+    if isinstance(values, list):
+        return values
+    return [values]
